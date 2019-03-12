11f5873371a84513908a7127c0df8ef9faefa24b
gnguarascio <42662834+gnguarascio@users.noreply.github.com>
2019-03-11 22:24:37 -0400

    Progress

--- Lab0x01-diagonals.asm
+++ Lab0x01-diagonals.asm
@@ -1,7 +1,15 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+; main area
+
+section .text
+    global _start
+_start:
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ; sys_read area
 
-%macro readC 1        ; defines reading global var
+%macro read 1         ; defines reading global var
 	mov EDX, 1000     ; # of bytes to be read
 	mov ECX, ???      ; address where read input is stored
 	mov EBX, 0        ; standard input
@@ -10,16 +18,28 @@
 %endmacro
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ; sys_write area
 
-%macro writeC 1
-	mov EDX, ???
-	mov ECX, ???
-	mov EBX, 1
-	mov EAX, 4
-	int 0x80
+%macro write 1        ; defines writing global var
+	mov EDX, ???      ; length of what's to be printed
+	mov ECX, ???      ; address of what's to be printed
+	mov EBX, 1        ; standard output
+	mov EAX, 4        ; code for sys_write
+	int 0x80          ; stop, do a sys_write
 %endmacro
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+; data area
+
+section .data
+
+    newline db 0x0A         ; newline character
+    space db 0x20           ; space character
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+; reservation area
+
+section .bss
+
+	; Not sure what to reserve yet! But here are my two new things.
+	

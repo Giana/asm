415d471a9e4ade07773a4d48a2a75f595ac2634c
gnguarascio <42662834+gnguarascio@users.noreply.github.com>
2019-03-13 01:10:58 -0400

    Progress

--- Lab0x01-hexdump.asm
+++ Lab0x01-hexdump.asm
@@ -1,1 +1,49 @@
-N/A
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+; sys_read area
+
+%macro read 1         ; defines reading global var
+	mov EDX, 1        ; # of bytes to be read
+	mov ECX, %1       ; address where read input is stored
+	mov EBX, 0        ; standard input
+	mov EAX, 3        ; code for sys_read
+	int 0x80          ; stop, do a sys_read
+%endmacro
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+; sys_write area
+
+%macro write 1        ; defines writing global var
+	mov EDX, 1        ; length of what's to be printed
+	mov ECX, %1       ; address of what's to be printed
+	mov EBX, 1        ; standard output
+	mov EAX, 4        ; code for sys_write
+	int 0x80          ; stop, do a sys_write
+%endmacro
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+; constant area
+
+columns equ 8         ; hard coded column #, easily changed
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+; data area
+
+section .data
+
+    newline: db 0xA          ; newline character
+    space: db 0x20           ; space character
+    
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+; reservation area
+
+section .bss
+
+    currChar: resd 1         ; 4 bytes resvd for current char
+    count: resd 1            ; 4 bytes resvd for counter
+    ;; laying some groundword
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+; exit area
+
+	mov eax, 0x1
+	xor ebx, ebx
+	int 0x80

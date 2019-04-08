98d64bea2255990d0958e0696cf19689fa825e3d
gnguarascio <42662834+gnguarascio@users.noreply.github.com>
2019-04-07 21:36:29 -0400

    Initial

--- Lab0x04-library.asm
+++ Lab0x04-library.asm
@@ -1,7 +1,5 @@
 section .text
 	global _replaceChar
-	global _sumAndPrintList
-	extern printf
 
 _replaceChar:
 	push ebp
@@ -34,45 +32,3 @@ replaceloop:
 
 	leave
 	ret
-
-hdr:	db "Numbers", 9, "Running Total", 0Ah, 0
-fmt:	db "%7d", 9, "%d", 0Ah, 0
-
-_sumAndPrintList:
-
-	push ebp
-	mov ebp, esp
-	push esi
-	push edi
-	push ebx
-
-	xor ebx, ebx				; i = 0
-	xor esi, esi				; sum = 0
-	mov edi, [ebp + 8]			; list
-
-	push hdr
-	call printf
-	add esp, 4
-
-; [ebp + 12] -- length
-
-.addloop:
-	add esi, [edi + 4*ebx]
-
-	push esi
-	push dword [edi + 4*ebx]
-	push fmt
-	call printf
-	add esp, 12
-
-	inc ebx
-	cmp ebx, [ebp + 12]			; length
-	jl .addloop
-
-	mov eax, esi
-	pop ebx
-	pop edi
-	pop esi
-
-	leave
-	ret 

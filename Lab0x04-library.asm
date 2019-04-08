a45b6360d00d20cf65b889476c2f5a3505ae8f96
gnguarascio <42662834+gnguarascio@users.noreply.github.com>
2019-04-07 22:57:37 -0400

    Final

---
+++ Lab0x04-library.asm
@@ -0,0 +1,38 @@
+section .text
+    global _replaceChar
+
+_replaceChar:
+
+    push ebp
+    mov ebp, esp
+    push esi
+    push ebx
+
+    xor	eax, eax
+    xor	edx, edx                ; i = 0
+
+    mov	esi, [ebp + 8]          ; textPtr
+    mov	bl, [ebp + 16]          ; chS
+    mov	cl, [ebp + 20]          ; chR
+
+replaceloop:
+
+    cmp	edx, [ebp + 12]     ; len
+    jge	.end                ; while i < len
+    cmp	[esi + edx], bl     ; if(textPtr[i] == chS)
+    jne	.noreplace
+    mov	[esi + edx], cl     ; textPtr[i] = chR
+    inc	eax                 ; chars replaced ++
+
+.noreplace:
+
+    inc	edx					    ; i++
+    jmp	replaceloop
+
+.end:
+
+    pop	ebx
+    pop	esi
+
+    leave
+    ret
